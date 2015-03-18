d890168157bf0cae62a5240b52979becba0d9904
Nvertigo <nvertigo67@gmail.com>
2015-03-18 15:17:06 +0100

    include/linux/sched.h: fix my derp from incomplete conflict resolving.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1361,8 +1361,6 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
-	unsigned long atomic_flags; /* Flags needing atomic access. */
-
 	pid_t pid;
 	pid_t tgid;
 
